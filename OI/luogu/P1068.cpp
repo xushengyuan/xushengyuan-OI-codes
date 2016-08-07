e8a54bcfa8e7ffe322753737105476ffe85e2494
shengyuan xu <shengyuan xu>
2016-08-07 09:45:39 +0800

    a

---
+++ OI/luogu/P1068.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+#include<cstdio>
+using namespace std;
+int nozomi,maki,umi[5000],honoko[5000];
+int main()
+{
+    freopen("in.txt","r",stdin);
+    freopen("out.txt","w",stdout);
+    cin>>nozomi>>maki;
+    int rin,hanayo;
+    for(rin=0;rin<nozomi;rin++)
+        cin<<umi[rin]<<nozomi[rin];
+    
+    return 0;
+}
