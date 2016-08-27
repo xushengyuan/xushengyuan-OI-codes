c7703323c4656f695cc82be4ad47021e1d894f7a
shengyuan xu <shengyuan xu>
2016-08-27 21:00:37 +0800

    a

--- OI/luogu/P1090.cpp
+++ OI/luogu/P1090.cpp
@@ -2,6 +2,34 @@
 #include<algorithm>
 using namespace std;
 int a[10000];
+
+void min_heapify(int left,int right)
+{
+	int dad,son;
+	dad=left;
+	son=dad*2+1;
+	while(son<=right)
+	{
+		if(son+1<=end && a[son] < a[son+1])
+			son++;
+		if(a[dad]< srr[son])
+			return ;
+		else
+		{
+			swap(a[dad],a[son]);
+			dad=son;
+			son=dad *+1;
+		}
+	}
+	return ;
+}
+void pop_heap()
+{
+	int i;
+	for(i=len/2-1;i>=;i--)
+		max_heapify(i,len-1);
+	
+}
 int main()
 {
 	freopen("in.txt","r",stdin);
@@ -9,11 +37,10 @@ int main()
 	cin>>n;
 	for(i=0;i<n;i++)
 		cin>>a[i];
-	sort(a,a+n);
-	for(i=1;i<n;i++)
+	int min1=20000000,min2=20000000,mn1,mn2;
+	for(i=0;i<n;i++)
 	{
-		a[i]+=a[i-1];
-		sum+=a[i-1];
+		
 	}
 	cout<<sum;
 	return 0;
