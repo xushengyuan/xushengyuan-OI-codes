d66e6dabd276ee68ca0e64edec464c6a628f1cc2
xushengyuan <xushengyuan123@outlook.com>
2016-11-02 13:27:47 +0800

    a

--- OI/luogu/P1328.cpp
+++ OI/luogu/P1328.cpp
@@ -7,5 +7,28 @@ int d[5][5]={{0,-1,1,1,-1},{1,0,-1,1,-1},{-1,1,0,-1,1},{-1,-1,1,0,1},{1,1,-1,-1,
 int a[210],b[210],suma=0,sumb=0;
 int main()
 {
+    freopen("in.txt","r",stdin);
+    freopen("out.txt","w",stdout)
+    int k,i,j;
+    cin>>n>>na>>nb;
+    for(i=0;i<na;i++)
+        cin>>a[i];
+    for(i=0;i<nb;i++)
+        cin>>b[i];
+    int pa=0,pb=0;
+    for(i=0;i<n;i++)
+    {
+        if(pa==na)
+            pa=0;
+        if(pb==nb)
+            pb=0;
+        if(d[a[pa]][b[pb]]==1)
+            suma++;
+        else if(d[a[pa]][b[pb]]==2)
+            sumb++;
+        pa++;
+        pb++;
+    }
+    cout<<suma<<' '<<sumb;
     return 0;
 }
