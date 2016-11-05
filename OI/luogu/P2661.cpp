6ecbd0a72020a473630e0a16bfdda8137d1b6aa6
xushengyuan <xushengyuan123@outlook.com>
2016-11-05 16:24:03 +0800

    a

---
+++ OI/luogu/P2661.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+int n;
+int sum=2147483647,maki[200010],kke[200010],s[200010];
+int tmp;
+int main()
+{
+    freopen("in.txt","r",stdin);
+    freopen("out.txt","w",stdout);
+    memset(kke,0,sizeof(int))
+    int i,j,t,flag,t2=0;
+    cin>>n;
+    for(i=1;i<n;i++)
+        cin>>maki[i];
+    for(i=1;i<n;i++)
+    {
+        if(kke[i]!=0)
+            continue;
+        t=i;
+        tmp=0;
+        flag=0;
+        while(!s[t])
+        {
+            kke[t]=tmp;
+            s[t]=i;
+            t=maki[t];
+            if(tmp>sum)
+            {
+                flag=1;
+                break;
+            }
+        }
+        if(s[t]!=i || flag)
+            continue;
+        t2=tmp-kke[t];
+        sum=min(sum,t2);
+    }
+    cout<<sum;
+    return 0;
+}
