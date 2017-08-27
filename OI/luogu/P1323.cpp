0712573257de2746ba6805e539cf9235af37d5ab
xushengyuan <xushengyuan123@outlook.com>
2017-08-27 21:48:27 +0800

    2017/8/27

---
+++ OI/luogu/P1323.cpp
@@ -0,0 +1,18 @@
+//Date:
+//OJ:
+//Problem:
+//Solution:
+//By:xushengyuan
+#include <bits/stdc++.h>
+#include </ext/pb_ds/priority_queue.hpp>
+using namespace std;
+__gnu_pbds::priority_queue<int,greater<int> >q;
+int main()
+{
+    #ifdef _DEBUG
+        freopen("in.txt","r",stdin);
+        freopen("out.txt","w",stdout);
+    #endif
+    
+    return 0;
+}
