fcc5f4208ae84b4e01a44dfb055a266a9b540602
xu shengyuan <xushengyuan123@outlook.com>
2017-09-26 11:48:11 +0800

    P1586

---
+++ OI/luogu/P1586.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include MAXN 185;
+using namespace std;
+int n;
+int sum[32768]
+int init()
+{
+	for(int MAXN=0;)
+	return ;
+}
+int main()
+{
+	freopen("in.txt","r",stdin);
+	cin>>n;
+	for(int t=0;t<n;t++)
+	{
+		
+	}
+	return 0;
+}
