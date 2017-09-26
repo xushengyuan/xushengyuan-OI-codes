ee242ff905fda5a4fb24dfbbef8c7383d76b81d1
xushengyuan <xushengyuan123@outlook.com>
2017-09-26 13:03:37 +0800

    P1586 complete

--- OI/luogu/P1586.cpp
+++ OI/luogu/P1586.cpp
@@ -1,20 +1,37 @@
+//Date:2017/9/26
+//OJ:luogu
+//Problem:1586
+//Solution:DP
+//By:xushengyuan
 #include <bits/stdc++.h>
-#include MAXN 185;
+#define MAXN 185
 using namespace std;
 int n;
-int sum[32768]
-int init()
+int sum[5][32769];
+void init()
 {
-	for(int MAXN=0;)
+	memset(sum,0,sizeof(sum));
+	sum[0][0]=1;
+	for(int i=1;i*i<=32768;i++)
+		for(int len=1;len<=4;len++)
+			for(int j=i*i;j<=32768;j++)
+				sum[len][j]+=sum[len-1][j-i*i];
 	return ;
 }
 int main()
 {
-	freopen("in.txt","r",stdin);
+	//freopen("in.txt","r",stdin);
+	//freopen("out.txt","w",stdout);
 	cin>>n;
+	init();
+	//for(int i=0;i<1000;i++)
+	//	cout <<sum[i]<<' ';
 	for(int t=0;t<n;t++)
 	{
-		
+		int tmp;
+		cin>>tmp;
+		cout <<sum[1][tmp]+sum[2][tmp]+sum[3][tmp]+sum[4][tmp]<<endl;
+
 	}
 	return 0;
 }
