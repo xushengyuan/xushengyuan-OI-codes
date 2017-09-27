47d4018adc5eadcd61b095f6952add45cefa2d81
xushengyuan <xushengyuan123@outlook.com>
2017-09-27 13:39:19 +0800

    P1022

---
+++ OI/luogu/P1022.cpp
@@ -0,0 +1,16 @@
+//Date:2017/9/27
+//OJ:luogu
+//Problem:1022
+//Solution:String
+//By:xushengyuan
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    #ifdef _DEBUG
+        freopen("in.txt","r",stdin);
+        freopen("out.txt","w",stdout);
+    #endif
+    
+    return 0;
+}
