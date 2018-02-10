694cb9e3f234ce2b1041c33d403cd818ce6e5028
xushengyuan <xushengyuan123@outlook.com>
2018-02-10 21:50:57 +0800

    P1525

--- OI/luogu/P1525.cpp
+++ OI/luogu/P1525.cpp
@@ -14,7 +14,17 @@ struct edge
 }edges[MAXN];
 bool operator <(edge a,edge b)
 {
-    return a.w<b.w;
+    return a.w>b.w;
+}
+int data[MAXN],_less[MAXN];
+int find(int x)
+{
+    return data[x]==x?x:data[x]=find(data[x]);
+}
+void uni(int x,int y)
+{
+    data[find(y)]=find(x);
+    return ;
 }
 int main()
 {
@@ -22,6 +32,36 @@ int main()
         freopen("in.txt","r",stdin);
         freopen("out.txt","w",stdout);
     #endif
-    
+    cin>>n>>m;
+    for (int i = 0; i < m; i++)
+        cin>>edges[i].u>>edges[i].v>>edges[i].w;
+    sort(edges,edges+m);
+    int p=0;
+    for(int i=1;i<=n;i++)
+    {
+        data[i]=i;
+        _less[i]=-1;
+    }
+    while(p<m)
+    {
+        if(find(edges[p].u)!=find(edges[p].v))
+        {
+            if(_less[edges[p].u]!=-1)
+                uni(edges[p].v,_less[edges[p].u]);
+            else
+                _less[edges[p].u]=edges[p].v;
+            if(_less[edges[p].v]!=-1)
+                uni(edges[p].u,_less[edges[p].v]);
+            else
+                _less[edges[p].v]=edges[p].u;
+        }
+        else
+        {
+            cout<<edges[p].w<<endl;
+            return 0;
+        }
+        p++;
+    }
+    cout<<0;
     return 0;
 }
